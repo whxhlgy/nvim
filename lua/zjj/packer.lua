082c498b2ecc15c8f8b22a458be2a0db294ae023
virtualZjj <zjj476778843@gmail.com>
2023-09-15 08:55:19 +0800

    ignore some files

--- lua/zjj/packer.lua
+++ lua/zjj/packer.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
   --
@@ -137,4 +150,7 @@ return require("packer").startup(function(use)
     "jose-elias-alvarez/null-ls.nvim",
     requires = { "nvim-lua/plenary.nvim" },
   })
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
