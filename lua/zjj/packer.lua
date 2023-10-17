f2d66e18d401721c31cb6c14e51b79a3b6a16baf
Junjie-Zhong <zjj476778843@gmail.com>
2023-10-17 14:52:30 +0800

    remove mason

--- lua/zjj/packer.lua
+++ lua/zjj/packer.lua
@@ -104,8 +104,6 @@ return require("packer").startup(function(use)
   -- ###################
   --
   use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
   }
   use('hrsh7th/cmp-nvim-lsp')
